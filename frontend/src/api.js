c412c748a6f6b3c3ec4bf119eb8d7964a1bb2541
Gheoală Alexandru - Iliuță <162614973+Agheoala19@users.noreply.github.com>
2026-09-06 18:48:34 +0300

    Update api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-    baseURL: 'http://task-mastr.onrender.com/api'
+    baseURL: 'https://task-mastr.onrender.com/api'
 })
 
 api.interceptors.request.use((config) => {
